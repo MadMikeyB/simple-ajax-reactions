51d3c67f797a9cecefc4c8248a6883c984d3657f
Michael <mikeybsaccount@gmail.com>
2016-07-29 19:13:46 +0100

    Rewrite some things

--- database.php
+++ database.php
@@ -1,62 +1,62 @@
 <?php
 
 class DB {
-	protected $dbhost;
-	protected $dbname;
-	protected $dbuser;
-	protected $dbpass;
+    protected static $instance = null;
 
-	public function __construct($dbhost, $dbname, $dbuser, $dbpass)
-	{
-		$this->host = $dbhost;
-		$this->name = $dbname;
-		$this->user = $dbuser;
-		$this->pass = $dbpass;
-	}
+    public function __construct() {}
+    public function __clone() {}
 
-	public function connect()
-	{
-		print_r($this); exit;
-		try {
-			$pdo = new PDO('mysql:host=' . $this->host . 'dbname=' . $this->name, $this->user, $this->pass);
-			return $pdo;
-		} catch (PDOException $e) {
-			$e->getMessage();
-		}
-	}
+ 	public static function instance()
+    {
+        if (self::$instance === null)
+        {
+            $args  = array(
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => FALSE,
+            );
+            $config = 'mysql:host='.getenv('dbhost').';dbname='.getenv('dbname').';charset='.getenv('dbchar');
+            self::$instance = new PDO($config, getenv('dbuser'), getenv('dbpass'), $args);
+        }
+        return self::$instance;
+    }
+
+    public static function __callStatic($method, $args)
+    {
+        return call_user_func_array(array(self::instance(), $method), $args);
+    }
+
 
 	public function query($query)
 	{
-		$pdo = self::connect();
-		$statement = $pdo->prepare($query);
+		$statement = static::instance()->prepare($query);
 		$statement->execute();
-
 		return $statement->fetchAll();
 	}
 
 	public function select()
 	{
-
+		// ...
 	}
 
 	public function count()
 	{
-
+		// ...
 	}
 
 	public function getLastInsertId()
 	{
-
+		// ...
 	}
 
 	public function save()
 	{
-
+		// ...
 	}
 
 	public function update()
 	{
-
+		// ...
 	}
 
 }
